9ff2c7557b6a016ed28b14fd29749ecd0dbdb9ac
evakdev <75202278+evakdev@users.noreply.github.com>
2021-11-02 19:44:55 +0330

    initial commit

---
+++ project/base.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from config import db
+
+engine = create_engine(db,echo=False)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
