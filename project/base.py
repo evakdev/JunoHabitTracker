7c3a36afcace78753b9ac6d69d6e7d39df71808c
evakdev <75202278+evakdev@users.noreply.github.com>
2021-11-02 19:46:43 +0330

    applied black

--- project/base.py
+++ project/base.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from config import db
 
-engine = create_engine(db,echo=False)
+engine = create_engine(db, echo=False)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
