d238d575a85fc5b038938ae2032c206ae1db3900
evakdev <75202278+evakdev@users.noreply.github.com>
2021-11-04 18:19:01 +0330

    added run.py, controllers ,moved stuff to base

--- project/base.py
+++ project/base.py
@@ -1,8 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from telegram.ext import Updater
 from sqlalchemy.ext.declarative import declarative_base
-from config import db
+from config import db, token
+import logging
 
+#SQLAlchemy
 engine = create_engine(db, echo=False)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+#Telegram
+updater = Updater(token=token)
+dispatcher = updater.dispatcher
+
+#Logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
---
+++ project/run.py
@@ -0,0 +1,18 @@
+import base
+import main
+import habitcreator
+
+
+
+def recreate_database():
+
+    base.Base.metadata.drop_all(base.engine)
+    base.Base.metadata.create_all(base.engine)
+    table_list=base.engine.table_names()
+    print("Tables:")
+    for table in table_list:
+        print(f"     {table}")
+
+
+recreate_database()
+base.updater.start_polling()
