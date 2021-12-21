f1c80282c8ab2bd979678e1533ac8b2f3f33ec02
evakdev <75202278+evakdev@users.noreply.github.com>
2021-12-21 09:29:12 +0330

    major changes

--- project/controllers/crud.py
+++ project/controllers/crud.py
@@ -2,33 +2,40 @@ from base import Session
 from models.models import User, Habit, Record, Method
 
 
-def create_habit(name, user, **kwargs):
-    with Session() as s:
+##### Create #####
+
+
+def create_habit(name, user, method):
+    with Session(expire_on_commit=False) as s:
+        if type(method) == Method:
+            s.merge(method)
+            method = method.id
+        if type(user) == User:
+            s.merge(user)
+            user = user.id
+
         habit = s.query(Habit).filter_by(name=name, user=user).one_or_none()
         if habit:
             return None
-        habit = Habit(name=name, user=user)
-        if kwargs.get("method_id"):
-            habit.method_id = kwargs.get("method_id")
+        habit = Habit(name=name, user=user, method=method)
         s.add(habit)
         s.commit()
         return habit
 
 
 def create_user(id, timezone):
-    with Session() as s:
+    with Session(expire_on_commit=False) as s:
         user = s.query(User).filter_by(id=id).one_or_none()
         if not user:
             user = User(id=id, timezone=timezone)
             s.add(user)
             s.commit()
 
-        s.refresh(user)
         return user
 
 
 def create_record(user_id, habit_id, date):
-    with Session() as s:
+    with Session(expire_on_commit=False) as s:
         record = (
             s.query(Record)
             .filter_by(user=user_id, habit=habit_id, date=date)
@@ -52,28 +59,14 @@ def create_method(*args, **kwargs):
     if not (type_ and duration):
         return None
 
-    with Session() as s:
+    with Session(expire_on_commit=False) as s:
         method = Method(type_, duration, **kwargs)
         s.add(method)
         s.commit()
         return method
 
 
-def add_method(habit, method):
-    with Session() as s:
-        habit = s.merge(habit)
-        method = s.merge(method)
-        if habit.method:
-            habit.method.delete()
-        habit.method = method.id
-        s.commit()
-        return habit
-
-
-def add_timezone(user, timezone):
-    with Session() as s:
-        s.query(User).filter_by(id=user.id).update({"timezone": timezone})
-        s.commit()
+##### Get #####
 
 
 def get_user(id):
@@ -90,34 +83,12 @@ def get_habit(id, user_id):
         return habit
 
 
-def find_habit_by_name(habit_name, user_id):
+def get_habit_by_name(habit_name, user_id):
     """Gets habit from db by user id and habit name.
     Returns none if user has no habit in that name.
     """
     with Session() as s:
         habit = s.query(Habit).filter_by(name=habit_name, user=user_id).one_or_none()
-        return habit
-
-
-def edit_habit(habit, *args, **kwargs):
-    """Works with either habit or its id.
-    will edit name, and/or method.
-    to edit method, provide new method's id or object."""
-    with Session() as s:
-        s.merge(habit)
-        if type(habit) == int:
-            habit = s.query(Habit).filter_by(id=id).one_or_none()
-        name = kwargs.get("name")
-        method = kwargs.get("method")
-        if name:
-            habit.name = name
-        if method:
-            method = method.id if type(method) == Method else method
-            old_method_id = habit.method
-            habit.method = method
-            old_method = s.query(Method).filter_by(id=old_method_id).one_or_none()
-            old_method.delete()
-        s.commit()
         return habit
 
 
@@ -137,6 +108,32 @@ def get_record(user_id, habit_id, date):
         return record
 
 
+##### Edit #####
+
+
+def edit_habit(habit, new_name=None, new_method=None):
+    """Works with either habit or its id.
+    will edit name, and/or method.
+    to edit method, provide new method's id or object."""
+    with Session() as s:
+        if type(habit) != int:
+            s.merge(habit)
+            habit = habit.id
+
+        if new_name:
+            s.query(Habit).filter_by(id=habit).update({"name": new_name})
+        if new_method:
+            old_method_id = s.query(Habit).filter_by(id=habit).one().method
+            new_method = new_method.id if type(new_method) == Method else new_method
+            s.query(Habit).filter_by(id=habit).update({"method": new_method})
+            s.query(Method).filter_by(id=old_method_id).delete()
+
+        s.commit()
+
+
+##### Delete #####
+
+
 def delete_record(record):
     with Session() as s:
         s.delete(record)
@@ -149,12 +146,13 @@ def delete_habit(habit, delete_records=True, delete_method=True):
             records = s.query(Record).filter_by(habit=habit.id)
             records.delete(synchronize_session=False) if records.count() else None
 
-        if delete_method:
-            method = s.query(Method).filter_by(id=habit.method)
-            method.delete(synchronize_session=False) if method.count() else None
-
+        method_id = habit.method
         habit = s.query(Habit).filter_by(id=habit.id).one_or_none()
         s.delete(habit)
+
+        if delete_method:
+            method = s.query(Method).filter_by(id=method_id)
+            method.delete(synchronize_session=False) if method.count() else None
         s.commit()
 
 
--- project/controllers/manager.py
+++ project/controllers/manager.py
@@ -1,76 +1,62 @@
 from telegram.ext.callbackqueryhandler import CallbackQueryHandler
-from telegram.ext.conversationhandler import ConversationHandler
 from telegram.ext.commandhandler import CommandHandler
 from telegram.ext.filters import Filters
 from telegram.ext.messagehandler import MessageHandler
 from telegram.inline.inlinekeyboardbutton import InlineKeyboardButton
 from telegram.inline.inlinekeyboardmarkup import InlineKeyboardMarkup
-from controllers.crud import edit_habit, delete_habit, add_method, find_habit_by_name
+from controllers.crud import edit_habit, delete_habit, get_habit_by_name
 from controllers.base import Conversation
 from controllers.methodcreator import MethodCreator
-from controllers.mixins import MainMenuMixin, ChooseHabitMixin
+from controllers.mixins import ChooseHabitMixin
+from controllers.mainkeys import manager
 
 methodcreator = MethodCreator()
 
 
-class Manager(MainMenuMixin, ChooseHabitMixin, Conversation):
+class Manager(ChooseHabitMixin, Conversation):
     def __init__(self):
         super().__init__()
-        self.handler = ConversationHandler(
-            entry_points=[
-                CommandHandler("manage", self.ask_habit),
-                CallbackQueryHandler(self.ask_habit, pattern=self.keys.id),
+        self.entry_points = [
+            CommandHandler(manager, self.ask_habit),
+            CallbackQueryHandler(self.ask_habit, pattern=self.keys.id),
+        ]
+        self.states = self.choose_habit_states | {
+            self.keys.answer1: [
+                CallbackQueryHandler(self.ask_rename, pattern=self.keys.rename),
+                methodcreator.handler,
+                CallbackQueryHandler(self.ask_delete, pattern=self.keys.delete),
+                self.main_menu_callback_state,
             ],
-            states=self.choose_habit_states
-            | self.main_menu_states
-            | {
-                self.keys.answer2: [
-                    CallbackQueryHandler(self.ask_rename, pattern=self.keys.rename),
-                    methodcreator.handler,
-                    CallbackQueryHandler(self.ask_delete, pattern=self.keys.delete),
-                    CallbackQueryHandler(self.main_menu, pattern=self.keys.main_menu),
-                ],
-                self.keys.answer3: [MessageHandler(Filters.text, self.get_rename)],
-                self.keys.answer4: [
-                    CallbackQueryHandler(
-                        self.get_delete, pattern=self.keys.deleteconfirm
-                    ),
-                    CallbackQueryHandler(self.ask_task, pattern=self.keys.id),
-                ],
-                self.keys.goback: [
-                    CallbackQueryHandler(self.ask_habit, pattern=self.keys.goback)
-                ],
-                self.keys.redo: [
-                    CallbackQueryHandler(self.ask_habit, pattern=self.keys.id)
-                ],
-                self.keys.main_menu: [
-                    CallbackQueryHandler(self.main_menu, pattern=self.keys.main_menu)
-                ],
-            },
-            fallbacks=[CommandHandler(self.keys.cancel, self.cancel)],
-            map_to_parent={self.keys.end: self.keys.main_menu},
-            name="Manager",
-        )
+            self.keys.answer2: [MessageHandler(Filters.text, self.get_rename)],
+            self.keys.answer3: [
+                CallbackQueryHandler(self.get_delete, pattern=self.keys.deleteconfirm),
+                CallbackQueryHandler(self.ask_task, pattern=self.keys.id),
+            ],
+            self.keys.answer4: [
+                CallbackQueryHandler(self.ask_habit, pattern=self.keys.id)
+            ],
+            self.keys.methodend: [
+                CallbackQueryHandler(self.get_method, pattern=self.keys.methodend)
+            ],
+        }
+        self.name = "Manager"
+        self.create_handler()
 
     def add_keys(self):
         super().add_keys()
-        self.keys.id = "manager"
+        self.keys.id = manager
         self.keys.answer1 = self.keys.id + "1"
         self.keys.answer2 = self.keys.id + "2"
         self.keys.answer3 = self.keys.id + "3"
         self.keys.answer4 = self.keys.id + "4"
-        self.keys.answer5 = self.keys.id + "5"
-        self.keys.redo = self.keys.id + "redo"
         self.keys.rename = self.keys.id + "rename"
         self.keys.delete = self.keys.id + "delete"
         self.keys.changemethod = self.keys.id + "changemethod"
         self.keys.deleteconfirm = self.keys.id + "deleteconfirm"
 
-    def main_menu(self, update, context):
-        super().main_menu(update, context)
-        return self.keys.end
-
     def ask_habit(self, update, context):
+        if self.user_doesnt_exist(update):
+            return self.redirect_to_timezone(update, context)
         return super().ask_habit(update, context)
 
     def get_habit(self, update, context):
@@ -95,50 +81,50 @@ class Manager(MainMenuMixin, ChooseHabitMixin, Conversation):
         ]
         keyboard = InlineKeyboardMarkup(buttons)
         update.callback_query.edit_message_text(text, reply_markup=keyboard)
-        return self.keys.answer2
+        return self.keys.answer1
 
     def ask_rename(self, update, context):
         text = f"Enter a new name for {self.habit.name}"
         update.callback_query.edit_message_text(text)
-        return self.keys.answer3
+        return self.keys.answer2
 
     def get_rename(self, update, context):
         self.new_name = "".join(update.message.text).capitalize()
         if self.habit_exists(self.new_name):
             return self.duplicate_habit(update, context)
         old_name = self.habit.name
-        edit_habit(self.habit, name=self.new_name)
-        print(self.habit.name)
+        edit_habit(self.habit, new_name=self.new_name)
         text = f"Successfully changed {old_name} to {self.new_name}."
         keyboard = InlineKeyboardMarkup(
             [[InlineKeyboardButton("↩ Go back", callback_data=self.keys.id)]]
         )
         update.message.reply_text(text, reply_markup=keyboard)
-        return self.keys.redo
+        return self.keys.answer4
 
     def duplicate_habit(self, update, context):
         text = (
             f"You already have a habit named {self.new_name}!\n"
             "Enter a new name, or go back to edit the other {self.new_name} instead.\n"
         )
-        buttons = [[InlineKeyboardButton("Go back", callback_data=self.keys.redo)]]
+        buttons = [[InlineKeyboardButton("Go back", callback_data=self.keys.answer4)]]
         keyboard = InlineKeyboardMarkup(buttons)
         update.message.reply_text(text, keyboard)
-        return self.keys.answer4
+        return self.keys.answer3
 
     def habit_exists(self, name):
-        habit = find_habit_by_name(name, self.user.id)
+        habit = get_habit_by_name(name, self.user.id)
         return bool(habit)
 
     def get_method(self, update, context):
         method = context.user_data.get("method")
-        add_method(self.habit, method)
+
+        edit_habit(self.habit, new_method=method)
         text = "Successfully changed your tracking method."
         keyboard = InlineKeyboardMarkup(
             [[InlineKeyboardButton("↩ Go back", callback_data=self.keys.id)]]
         )
         update.callback_query.edit_message_text(text, reply_markup=keyboard)
-        return self.keys.redo
+        return self.keys.answer4
 
     def ask_delete(self, update, context):
         text = (
@@ -155,13 +141,13 @@ class Manager(MainMenuMixin, ChooseHabitMixin, Conversation):
         ]
         keyboard = InlineKeyboardMarkup(buttons)
         update.callback_query.edit_message_text(text, reply_markup=keyboard)
-        return self.keys.answer4
+        return self.keys.answer3
 
     def get_delete(self, update, context):
         name = self.habit.name
         delete_habit(self.habit)
         self.habit = None
         text = f"Successfully deleted {name} and all its records."
-        keyboard = InlineKeyboardMarkup(self.main_menu_button)
+        keyboard = InlineKeyboardMarkup([self.main_menu_button])
         update.callback_query.edit_message_text(text, reply_markup=keyboard)
         return self.keys.main_menu
