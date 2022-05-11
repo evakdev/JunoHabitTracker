e9e90a2859eeb407ce57d1bf6f1309395c5b2b1d
evakdad <evakdad@gmail.com>
2022-05-11 21:28:01 +0430

    -

--- project/base.py
+++ project/base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from telegram.ext import Updater
 from sqlalchemy.ext.declarative import declarative_base
-from config import db_url, token
+
 import logging
 
 
